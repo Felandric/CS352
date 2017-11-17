20078a7e25d3c75e2a28f00b4b2d32b34b5563c8
rand0mnpc <32993377+rand0mnpc@users.noreply.github.com>
2017-11-17 18:28:58 -0500

    gud progress

--- sock352.py
+++ sock352.py
@@ -72,6 +72,11 @@ def readKeyChain(filename):
                 # hash, we may have a valid host/key pair in the keychain
                 if ((len(words) >= 4) and (words[0].find("#") == -1)):
                     host = words[1]
+
+                    # added this to correct resolving of localhost
+                    if host == "127.0.0.1":
+                        host = "localhost"
+
                     port = words[2]
                     keyInHex = words[3]
                     if (words[0] == "private"):
@@ -147,24 +152,24 @@ class socket:
         while not acked:
             try:
                 self.sock.sendto(SYN_Packet, self.serv_addr)
-                #print("Connection request sent")
+                print("Connection request sent")
                 ack = self.sock.recvfrom(header_len)[0]
                 acked = True
             except syssock.timeout:
-                #print("Timeout occurred. Resending...")
+                print("Timeout occurred. Resending...")
                 pass
         
         # Receive SYN ACK (B)
         self.last_pkt_recvd = struct.unpack('!BBBBHHLLQQLL', ack)
-        #print("Server response received")
+        print("Server response received")
         if self.last_pkt_recvd [1] == SOCK352_RESET:
-            #print("Connection Refused")
+            print("Connection Refused")
             pass
         elif self.last_pkt_recvd [1] == SOCK352_SYN | SOCK352_ACK:
-            #print("Connection Successful")
+            print("Connection Successful")
             pass
         else:
-            #print("Server response invalid")
+            print("Server response invalid")
             pass
         return
 
@@ -193,7 +198,7 @@ class socket:
             opt_ptr = 0b0
 
         self.last_pkt_recvd = struct.unpack('!BBBBHHLLQQLL', SYN_Packet)
-        #print("Connection request received")
+        print("Connection request received")
 
         # Send SYN ACK (B)
         version = 0x1
@@ -216,7 +221,7 @@ class socket:
         connection_response = self.udpPkt_hdr_data.pack(version, flags, opt_ptr, protocol, header_len, checksum,
                                                         source_port, dest_port, sequence_no, ack_no, window, payload_len)
         self.sock.sendto(connection_response, self.client_addr)  # send initial packet over the connection
-        #print("Server response sent")
+        print("Server response sent")
         return self, self.client_addr
 
     def close(self):
@@ -244,11 +249,11 @@ class socket:
                     self.sock.sendto(connection_response, self.client_addr)
                 elif self.amServer == False:
                     self.sock.sendto(connection_response, self.serv_addr)
-                #print("Termination request sent")
+                print("Termination request sent")
                 ack = self.sock.recvfrom(header_len)[0]
                 acked = True
             except syssock.timeout:
-                #print("Timeout occurred. Resending...")
+                print("Timeout occurred. Resending...")
                 pass                                           
         
 
@@ -257,21 +262,29 @@ class socket:
         if FIN_ACK[1] == SOCK352_FIN:
             flags |= SOCK352_ACK
             self.sock.close()
-            #print("Connection terminated")
+            print("Connection terminated")
         else:
-            #print("Server response invalid")
+            print("Server response invalid")
             pass
         return
 
     def send(self, buffer):
+        plaintext_len = len(buffer)
+
         if len(buffer) == 0:
             return 0
 
         buffer = buffer[:4000]  # added to solve fragmentation problem
-        
+
+        if self.isEncrypted == True:
+            buffer = self.box.encrypt(buffer)
+            opt_ptr = 0b1
+        else:
+            opt_ptr = 0b0
+
         version = 0x1
         flags = 0
-        opt_ptr = 0
+        #opt_ptr = 0
         protocol = 0
         header_len = struct.calcsize('!BBBBHHLLQQLL')
         checksum = 0
@@ -282,10 +295,8 @@ class socket:
         window = 0
         payload_len = len(buffer)
 
-        
         bytes_to_send = len(buffer)
         bytessent = 0
-
         if len(buffer) <= MTU:
             # packs header data into a string suitable to be sent over transmitting socket
             header = self.udpPkt_hdr_data.pack(version, flags, opt_ptr, protocol, header_len, checksum,
@@ -299,21 +310,21 @@ class socket:
                     if self.amServer == True:
                         bytessent = self.sock.sendto((header + buffer), self.client_addr)
                     elif self.amServer == False:
-                        bytessent = self.sock.sendto((header + buffer), self.serv_addr)
-                    #print("%i byte payload sent. SEQNO = %d. Awaiting ACK..." % (len(buffer), sequence_no))
+                          bytessent = self.sock.sendto((header + buffer), self.serv_addr)
+                    print("%i byte payload sent. SEQNO = %d. Awaiting ACK..." % (len(buffer), sequence_no))
                     ack = self.sock.recvfrom(header_len)[0]
                     acked = True
                 except syssock.timeout:
-                    #print("Timeout occurred. Resending...")
+                    print("Timeout occurred. Resending...")
                     pass
 
             # receive ACK
             self.last_pkt_recvd = struct.unpack('!BBBBHHLLQQLL', ack)
             if (self.last_pkt_recvd[1] == SOCK352_ACK) and (self.last_pkt_recvd[9] == sequence_no):
-                #print("ACK received")
+                print("ACK received")
                 pass
             else:
-                #print("Response invalid")
+                print("Response invalid")
                 pass
             bytessent = bytessent - header_len
 
@@ -326,21 +337,38 @@ class socket:
                 bytessent = bytessent + self.send(buffer[bytessent:bytessent+payload_len])
                 bytes_to_send = bytes_to_send - payload_len
                 
+
+        if self.isEncrypted == True:
+            bytessent -= 40
+
         return bytessent # subtract header size
 
     def recv(self, nbytes):
+
+        if self.isEncrypted == True:
+            nbytes += 40
+
         header_len = struct.calcsize('!BBBBHHLLQQLL')
         payload = ''
         payload_len = nbytes
         # receive packet
         if nbytes <= MTU:
+
             packet = self.sock.recvfrom(nbytes + header_len)[0]
+
             self.last_pkt_recvd = struct.unpack('!BBBBHHLLQQLL', packet[:header_len]) # read header
             payload = packet[header_len:header_len+self.last_pkt_recvd[11]]  # extract payload
+
+            if self.isEncrypted == True:
+                payload = self.box.decrypt(payload)
+                opt_ptr = 0b1
+            else:
+                opt_ptr = 0b0
+
             # send ACK
             version = 0x1
             flags = SOCK352_ACK
-            opt_ptr = 0
+            #opt_ptr = 0
             protocol = 0
             header_len = struct.calcsize('!BBBBHHLLQQLL')
             checksum = 0
@@ -350,7 +378,7 @@ class socket:
             ack_no = self.last_pkt_recvd[8]
             window = 0
             
-            #print("%i byte payload received. SEQNO = %d. Sending ACK..." % (payload_len, ack_no))
+            print("%i byte payload received. SEQNO = %d. Sending ACK..." % (payload_len, ack_no))
             
             payload_len = header_len
             
